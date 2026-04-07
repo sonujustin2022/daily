92610a3565d04904a62d526bf85c451529218df8
sonujustin2022 <sonujustin6282@gmail.com>
2026-04-08 00:31:26 +0530

    task04 finshed

--- task04.js
+++ task04.js
@@ -8,4 +8,23 @@
 // 👉 Try:
 
 // Normal loop (recommended)
+
+// const arrSum = (arr)=>{
+//     let sum = 0;
+
+//     for(let value of arr){
+//         sum += value;
+//     }
+//     return sum;
+// }
+
+// console.log(arrSum([1,2,3]))
+
+
 // Then maybe reduce()
+
+const arrSum = (arr) =>{
+  return  arr.reduce((accu,curr)=>accu+curr,0);
+}
+
+console.log(arrSum([1,2,4]))
