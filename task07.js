d9c26ee4cc0b7986b96a765f1e3803d3bea8d631
sonujustin2022 <sonujustin6282@gmail.com>
2026-04-08 17:43:37 +0530

    task07 finished

--- task07.js
+++ task07.js
@@ -3,3 +3,24 @@
 // // Output: 3
 
 // 👉 Use your countFreq logic.
+
+const firstUnique = (arr) =>{
+    let repeatValue = {};
+    for(let value of arr){
+        if(Object.hasOwn(repeatValue,value)){
+        repeatValue[value] += 1;    
+
+        }else {
+            repeatValue[value] = 1;
+        }
+    }
+
+    for(let value of arr){
+        if(repeatValue[value] ===1){
+            return value;
+        }
+    } 
+    return "no repeat value"
+}
+
+console.log(firstUnique([1, 2, 2, 3, 1, 4]))
